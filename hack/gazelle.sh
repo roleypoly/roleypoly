74348afa4753abfb51c2bc134488fb9089f1e5f8
Katalina Okano <git@kat.cafe>
2020-09-23 21:13:02 -0400

    chore: fix an intermittent gazelle race

--- hack/gazelle.sh
+++ hack/gazelle.sh
@@ -5,6 +5,7 @@ cd `dirname $(realpath $0)`
 bazel run //:gazelle
 bazel run //:gazelle -- update-repos -from_file=./go.mod --to_macro=deps.bzl%go_repositories -prune=true
 
+sleep 0.5
 echo "Fixing deps.bzl..."
 head -n2 ../deps.bzl > ../deps.bzl~
 tail -n+3 ../deps.bzl | sed '/^$/d' >> ../deps.bzl~
