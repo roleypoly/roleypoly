d0afb1488e9a768802255d876c85173d9de71bba
Katalina Okano <git@kat.cafe>
2020-10-10 03:54:58 -0400

    feat(design-system): convert popover story

---
+++ src/design-system/atoms/popover/Popover.stories.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Button } from "roleypoly/src/design-system/atoms/button";
+import { Popover as PopoverComponent } from "./Popover";
+
+export default {
+  title: "Atoms/Popover",
+  argTypes: {
+    canDefocus: { control: "boolean" },
+  },
+  args: {
+    canDefocus: true,
+  },
+};
+
+export const Popover = ({ canDefocus }) => {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  return (
+    <div style={{ padding: 50 }}>
+      <Button size="small" onClick={() => setIsOpen(!isOpen)}>
+        {!isOpen ? "Open" : "Close"} me!
+      </Button>
+      <PopoverComponent
+        position="top right"
+        active={isOpen}
+        onExit={() => setIsOpen(false)}
+        canDefocus={canDefocus}
+        headContent={<>Hello c:</>}
+      >
+        stuff
+      </PopoverComponent>
+    </div>
+  );
+};
