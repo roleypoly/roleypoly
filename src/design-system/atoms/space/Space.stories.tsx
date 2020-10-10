f7e2d1afef34d973dcec7446705446a151843cff
Katalina Okano <git@kat.cafe>
2020-10-10 04:19:07 -0400

    feat(design-system): add space stories

---
+++ src/design-system/atoms/space/Space.stories.tsx
@@ -0,0 +1,14 @@
+import { Space as SpaceComponent } from "./Space";
+import * as React from "react";
+
+export default {
+  title: "Atoms",
+};
+
+export const Space = () => (
+  <>
+    hello world
+    <SpaceComponent />
+    but im over here
+  </>
+);
