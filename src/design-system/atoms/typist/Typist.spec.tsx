ba558ecf91090699e5792ae4b4ae330b542b36b5
Katalina Okano <git@kat.cafe>
2020-10-24 21:26:43 -0400

    chore(design-system): cleanup typist tests

--- src/design-system/atoms/typist/Typist.spec.tsx
+++ src/design-system/atoms/typist/Typist.spec.tsx
@@ -1,6 +1,5 @@
-import { mount, shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 import * as React from 'react';
-import { act } from 'react-dom/test-utils';
 import { Typist } from './Typist';
 
 jest.useFakeTimers();
