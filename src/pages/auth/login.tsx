bd15d1f6fa72ea7ad3dba92d70876d12ba704768
Katalina Okano <git@kat.cafe>
2020-12-06 07:34:25 -0500

    fix auth/login base url resolution failure

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,19 +1,24 @@
+import { NextPageContext } from 'next';
 import getConfig from 'next/config';
 import * as React from 'react';
 import { AuthLogin } from 'roleypoly/design-system/templates/auth-login';
 
-const { publicRuntimeConfig } = getConfig();
-
-const loginPage = () => {
+const loginPage = (props: { apiRoot: string }) => {
     const onSendSecretCode = (code: string) => {
         console.log(code);
     };
     return (
         <AuthLogin
             onSendSecretCode={onSendSecretCode}
-            discordOAuthLink={`${publicRuntimeConfig.apiPublicURI}/login-bounce`}
+            discordOAuthLink={`${props.apiRoot}/login-bounce`}
         />
     );
 };
 
 export default loginPage;
+
+loginPage.getInitialProps = (context: NextPageContext) => {
+    const { publicRuntimeConfig } = getConfig();
+
+    return { apiRoot: publicRuntimeConfig.apiPublicURI };
+};
