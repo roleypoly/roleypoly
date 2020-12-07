d8bda6fb43a3436af25c306b4f7d91219ec12554
Katalina Okano <git@kat.cafe>
2020-12-06 22:36:11 -0500

    try pulling secrets from gcloud for tf

---
+++ terraform/mappings.tf
@@ -0,0 +1,12 @@
+locals {
+  artifactBaseMap = {
+    us-east4             = "us-docker.pkg.dev/roleypoly/roleypoly/"
+    us-central1          = "us-docker.pkg.dev/roleypoly/roleypoly/"
+    us-west1             = "us-docker.pkg.dev/roleypoly/roleypoly/"
+    europe-west2         = "europe-docker.pkg.dev/roleypoly/roleypoly/"
+    europe-west3         = "europe-docker.pkg.dev/roleypoly/roleypoly/"
+    australia-southeast1 = "asia-docker.pkg.dev/roleypoly/roleypoly/"
+    asia-northeast1      = "asia-docker.pkg.dev/roleypoly/roleypoly/"
+    asia-southeast1      = "asia-docker.pkg.dev/roleypoly/roleypoly/"
+  }
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -32,11 +32,6 @@ terraform {
   }
 }
 
-variable "cloudflare_email" {
-  type      = string
-  sensitive = true
-}
-
 variable "cloudflare_api_token" {
   type      = string
   sensitive = true
@@ -53,7 +48,6 @@ variable "cloudflare_zone_id" {
 }
 
 provider "cloudflare" {
-  email      = var.cloudflare_email
   api_token  = var.cloudflare_api_token
   account_id = var.cloudflare_account_id
 }
