5d17105eba17d7b2afda84be3a5ab4e004008689
Katalina Okano <git@kat.cafe>
2020-12-08 01:02:53 -0500

    load auth login ui config from server only

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,26 +1,25 @@
-import { NextPageContext } from 'next';
+import { GetServerSidePropsContext } from 'next';
 import getConfig from 'next/config';
 import * as React from 'react';
 import { AuthLogin } from 'roleypoly/design-system/templates/auth-login';
 
-const { publicRuntimeConfig } = getConfig();
-
-const loginPage = (props: { apiRoot: string }) => {
-    const onSendSecretCode = (code: string) => {
-        console.log(code);
-    };
+const loginPage = (props: { apiURI: string }) => {
     return (
         <AuthLogin
-            onSendSecretCode={onSendSecretCode}
-            discordOAuthLink={`${
-                props.apiRoot || publicRuntimeConfig.apiPublicURI
-            }/login-bounce`}
+            onSendSecretCode={() => {}}
+            discordOAuthLink={`${props.apiURI}/login-bounce`}
         />
     );
 };
 
 export default loginPage;
 
-loginPage.getInitialProps = (context: NextPageContext) => {
-    return { apiRoot: publicRuntimeConfig.apiPublicURI };
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    const { publicRuntimeConfig } = getConfig();
+
+    return {
+        props: {
+            apiURI: publicRuntimeConfig.apiPublicURI,
+        },
+    };
 };
