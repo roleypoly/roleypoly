447876be32cbee58a0e5713f248e80ee19e03b8f
Katalina Okano <git@kat.cafe>
2020-12-17 16:54:56 -0500

    chore: remove mustAuthenticate from get-session

--- src/backend-worker/handlers/get-session.ts
+++ src/backend-worker/handlers/get-session.ts
@@ -1,17 +1,12 @@
 import { SessionData } from 'roleypoly/common/types';
 import { respond, withSession } from '../utils/api-tools';
 
-export const GetSession = withSession(
-    (session?: SessionData) => (): Response => {
-        const { user, guilds, sessionID } = session || {};
+export const GetSession = withSession((session?: SessionData) => (): Response => {
+    const { user, guilds, sessionID } = session || {};
 
-        return respond({
-            user,
-            guilds,
-            sessionID,
-        });
-    },
-    {
-        mustAuthenticate: true,
-    }
-);
+    return respond({
+        user,
+        guilds,
+        sessionID,
+    });
+});
