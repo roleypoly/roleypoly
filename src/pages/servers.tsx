51ae64e6640023602242070cbcccb7428727ef58
Katalina Okano <git@kat.cafe>
2020-12-18 21:02:07 -0500

    chore: add placeholder /servers with appshell

--- src/pages/servers.tsx
+++ src/pages/servers.tsx
@@ -1,1 +1,11 @@
-export default () => <div></div>;
+import { AppShell } from 'roleypoly/design-system/organisms/app-shell';
+import { useAppShellProps } from 'roleypoly/providers/appShellData';
+
+export default () => {
+    const { appShellProps } = useAppShellProps();
+    return (
+        <AppShell {...appShellProps}>
+            <div></div>
+        </AppShell>
+    );
+};
