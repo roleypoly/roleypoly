7f031bb9609cb90cbe8d8185ff32f6ed1ce23938
Katalina Okano <git@kat.cafe>
2020-12-21 05:10:57 -0500

    chore: prefix un-consumed router.replace() calls with void
    Signed-off-by: Katalina Okano <git@kat.cafe>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
 
     React.useEffect(() => {
         if (user || guilds) {
-            router.replace('/servers');
+            void router.replace('/servers');
         }
     }, [guilds, user]);
 
