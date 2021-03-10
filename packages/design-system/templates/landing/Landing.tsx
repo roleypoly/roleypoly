40f577d01d4fb6cb9ff57f3f8cc04793d76bfacb
Katalina Okano <git@kat.cafe>
2021-03-10 00:12:01 -0500

    fix linting issues

--- packages/design-system/templates/landing/Landing.tsx
+++ packages/design-system/templates/landing/Landing.tsx
@@ -1,9 +1,8 @@
 import { AppShell } from '@roleypoly/design-system/organisms/app-shell';
 import { Landing } from '@roleypoly/design-system/organisms/landing';
 import * as React from 'react';
-import { ProvidableAppShellProps } from 'roleypoly/providers/appShellData';
 
-export const LandingTemplate = (props: ProvidableAppShellProps) => (
+export const LandingTemplate = (props: any) => (
     <AppShell showFooter {...props}>
         <Landing />
     </AppShell>
