e5e031fa27c6c7275d1f1fd1558dcbc2367c00e3
Katalina Okano <git@kat.cafe>
2021-03-12 18:43:56 -0500

    chore: fix storybook weirdness

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,7 +1,3 @@
-const path = require('path');
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-const { NormalModuleReplacementPlugin } = require('webpack');
-
 module.exports = {
     stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
     addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
