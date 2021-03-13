fac361d277dcf22e54fa1287e3fedc3a8968a775
Katalina Okano <git@kat.cafe>
2021-03-13 05:16:25 -0500

    chore(web): memoize default url since it'll mostly always be the same no matter what

--- packages/web/src/api-context/getDefaultApiUrl.ts
+++ packages/web/src/api-context/getDefaultApiUrl.ts
@@ -1,14 +1,18 @@
-export const getDefaultApiUrl = (host: string = window.location.hostname) => {
-    if (/^roleypoly.com$/.test(host)) {
-        return 'https://api-prod.roleypoly.com';
-    } else if (
-        /roleypoly\.pages\.dev$/.test(host) ||
-        /^stage.roleypoly.com$/.test(host)
-    ) {
-        return 'https://api-stage.roleypoly.com';
-    } else if (/\blocalhost|127\.0\.0\.1\b/.test(host)) {
-        return 'http://localhost:6609';
-    } else {
-        return 'https://api-prod.roleypoly.com';
+import * as memoizeOne from 'memoize-one';
+
+export const getDefaultApiUrl = memoizeOne.default(
+    (host: string = window.location.hostname) => {
+        if (/^roleypoly.com$/.test(host)) {
+            return 'https://api-prod.roleypoly.com';
+        } else if (
+            /roleypoly\.pages\.dev$/.test(host) ||
+            /^stage.roleypoly.com$/.test(host)
+        ) {
+            return 'https://api-stage.roleypoly.com';
+        } else if (/\blocalhost|127\.0\.0\.1\b/.test(host)) {
+            return 'http://localhost:6609';
+        } else {
+            return 'https://api-prod.roleypoly.com';
+        }
     }
-};
+);
