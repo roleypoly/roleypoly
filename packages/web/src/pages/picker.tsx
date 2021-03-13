520b1c46fef7599aee08fcc4635d5b55669c42fc
Katalina Okano <git@kat.cafe>
2021-03-13 17:05:00 -0500

    feat(web): add server-setup page for when bot isn't in the server picked

--- packages/web/src/pages/picker.tsx
+++ packages/web/src/pages/picker.tsx
@@ -1,5 +1,6 @@
 import { Redirect } from '@reach/router';
 import { RolePickerTemplate } from '@roleypoly/design-system/templates/role-picker';
+import { ServerSetupTemplate } from '@roleypoly/design-system/templates/server-setup';
 import { PresentableGuild, RoleUpdate, UserGuildPermissions } from '@roleypoly/types';
 import * as React from 'react';
 import { useSessionContext } from '../session-context/SessionContext';
@@ -12,13 +13,20 @@ type PickerProps = {
 const Picker = (props: PickerProps) => {
     const { session, authedFetch, isAuthenticated } = useSessionContext();
 
-    const [pickerData, setPickerData] = React.useState<PresentableGuild | null>(null);
+    const [pickerData, setPickerData] = React.useState<PresentableGuild | null | false>(
+        null
+    );
     const [pending, setPending] = React.useState(false);
 
     React.useEffect(() => {
         const fetchPickerData = async () => {
             const response = await authedFetch(`/get-picker-data/${props.serverID}`);
             const data = await response.json();
+
+            if (response.status !== 200) {
+                setPickerData(false);
+                return;
+            }
 
             setPickerData(data);
         };
@@ -32,6 +40,26 @@ const Picker = (props: PickerProps) => {
 
     if (pickerData === null) {
         return <div>Loading...</div>;
+    }
+
+    if (pickerData === false) {
+        if (session && session.user && session.guilds) {
+            const guildSlug = session.guilds.find((guild) => guild.id === props.serverID);
+            if (!guildSlug) {
+                throw new Error('placeholder: guild not found in user slugs, 404');
+            }
+
+            return (
+                <ServerSetupTemplate
+                    activeGuildId={props.serverID}
+                    user={session.user}
+                    guilds={session.guilds || []}
+                    guildSlug={guildSlug}
+                />
+            );
+        }
+
+        throw new Error('placeholder: session state is odd, 404');
     }
 
     const onSubmit = async (submittedRoles: string[]) => {
