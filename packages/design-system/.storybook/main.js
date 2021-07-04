17bceae60f45f4e4b4fe62aba565224f7d9ddc2c
Katalina Okano <git@kat.cafe>
2021-07-04 13:17:13 -0500

    cleanup storybook

--- packages/design-system/.storybook/main.js
+++ packages/design-system/.storybook/main.js
@@ -1,9 +1,9 @@
 module.exports = {
   stories: [
+    '../*.stories.mdx',
     ...['atoms', 'molecules', 'organisms', 'templates'].map(
       (dir) => `../${dir}/**/*.stories.@(tsx|mdx)`
     ),
-    '*.stories.mdx',
   ],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   reactOptions: {
