5f7f4c05a5ee79a073e961c31a5474de5f3458ed
Katalina Okano <git@kat.cafe>
2023-05-22 00:25:24 -0400

    something is wrong with CI :(

--- packages/api/src/utils/id.ts
+++ packages/api/src/utils/id.ts
@@ -1,4 +1,6 @@
-import { decodeTime, ulid } from 'ulid-workers';
+import { decodeTime, ulidFactory } from 'ulid-workers';
+
+const ulid = ulidFactory();
 
 export const getID = () => ulid();
 export const dateFromID = (id: string) => decodeTime(id);
